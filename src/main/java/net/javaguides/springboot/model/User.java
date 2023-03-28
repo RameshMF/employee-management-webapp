568c3458706614ff65477f9156c2ea9cf0b26842
Ramesh Fadatare <ramesh24fadatare@gmail.com>
2023-03-28 08:43:42 +0530

    Upgraded to spring boot 3

--- src/main/java/net/javaguides/springboot/model/User.java
+++ src/main/java/net/javaguides/springboot/model/User.java
@@ -2,18 +2,7 @@ package net.javaguides.springboot.model;
 
 import java.util.Collection;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-import javax.persistence.JoinColumn;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name =  "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
